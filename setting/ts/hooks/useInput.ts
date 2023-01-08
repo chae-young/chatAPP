207c38f3e02f3787bc3fbc0e0c820f597bacf1f0
이챙 <cycy8527@gmail.com>
2023-01-08 22:14:32 +0900

    Feat: login , signUp

---
+++ setting/ts/hooks/useInput.ts
@@ -0,0 +1,14 @@
+import React, { Dispatch, SetStateAction, useCallback, useState } from 'react';
+
+type ReturnTypes<T = any> = [T, (e:any) => void, Dispatch<SetStateAction<T>>]
+
+const useInput = <T>(initialData: T): ReturnTypes=> {
+    const [value,setValue] = useState(initialData)
+    const handler = useCallback((e)=>{
+        setValue(e.target.value)
+    },[])
+
+    return [value,handler,setValue]
+};
+
+export default useInput;
---
+++ setting/ts/pages/SignUp/index.tsx
@@ -0,0 +1,98 @@
+import useInput from '@hooks/useInput';
+import axios from 'axios';
+import React, { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } from './style';
+
+const SignUp = () => {
+    const [email,onChangeEmail,setEmail] = useInput('');
+    const [nickname,onChangeNickname,setNickname] = useInput('')
+    const [password,,setPassword] = useInput('');
+    const [passwordCheck,,setPasswordCheck] = useInput('');
+    const [mismatchError,setMismatchError] = useState(false);
+    const [signUpError,setSingUpError] = useState('')
+    const [signUpSuccess,setSingUpSuccess] = useState(false)
+
+    const onChangePassword = useCallback((e)=>{
+        setPassword(e.target.value)
+        setMismatchError(e.target.value !== passwordCheck)
+    },[[passwordCheck]])
+
+    const onChangePasswordCheck = useCallback((e)=>{
+        setPasswordCheck(e.target.value)
+        setMismatchError(e.target.value !== password)
+    },[[password]])
+
+    const onSubmit = useCallback((e)=>{
+        e.preventDefault();
+        if(!mismatchError && nickname){
+            setSingUpError('')
+            setSingUpSuccess(false)
+            console.log(email,nickname,password,passwordCheck)
+            axios.post('http://localhost:3095/api/users',{
+                email,nickname,password
+            })
+            .then((response) => {
+                console.log(response)
+                setSingUpSuccess(true)
+
+            })
+            .catch((error) => {
+                console.log(error.response)
+                setSingUpError(error.response.data)
+            })
+            .finally(() => { //성공 실패 둘다의 경우
+
+            })
+        }
+    },[email,nickname,password,passwordCheck])
+
+    return (
+        <div id="container">
+        <Header>Sleact</Header>
+        <Form onSubmit={onSubmit}>
+          <Label id="email-label">
+            <span>이메일 주소</span>
+            <div>
+              <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+            </div>
+          </Label>
+          <Label id="nickname-label">
+            <span>닉네임</span>
+            <div>
+              <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+            </div>
+          </Label>
+          <Label id="password-label">
+            <span>비밀번호</span>
+            <div>
+              <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+            </div>
+          </Label>
+          <Label id="password-check-label">
+            <span>비밀번호 확인</span>
+            <div>
+              <Input
+                type="password"
+                id="password-check"
+                name="password-check"
+                value={passwordCheck}
+                onChange={onChangePasswordCheck}
+              />
+            </div>
+            {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+            {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+            {signUpError && <Error>{signUpError}</Error>}
+            {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
+          </Label>
+          <Button type="submit">회원가입</Button>
+        </Form>
+        <LinkContainer>
+          이미 회원이신가요?&nbsp;
+          <Link to="/login">로그인 하러가기</Link>
+        </LinkContainer>
+      </div>
+    );
+};
+
+export default SignUp;
